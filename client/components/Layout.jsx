7762d8e570437cf0591a6d32c832aaca32dc23fc
Andrew Dushane <andrewdushane@gmail.com>
2016-03-14 23:34:59 -0400

    fix nav links

--- client/components/Layout.jsx
+++ client/components/Layout.jsx
@@ -2,13 +2,15 @@ import React, { Component } from 'react';
 
 import { Grid, Row, Nav, NavItem } from 'react-bootstrap';
 
+import { LinkContainer } from 'react-router-bootstrap';
+
 export default class Layout extends Component {
   render() {
     return (
       <Grid fluid>
         <Nav bsStyle="pills" activeKey={1}>
-          <NavItem eventKey={1} href="/">Home</NavItem>
-          <NavItem eventKey={2} href="/about" title="About">About</NavItem>
+          <LinkContainer to="/"><NavItem eventKey={1}>Home</NavItem></LinkContainer>
+          <LinkContainer to="/about"><NavItem eventKey={2} title="About">About</NavItem></LinkContainer>
         </Nav>
         <Row>
           {this.props.children}
